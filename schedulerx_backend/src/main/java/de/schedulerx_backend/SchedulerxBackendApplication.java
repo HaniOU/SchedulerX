3cf7b1c0326b7dc99446e0ec531e3a5dc9427122
HaniOU <ouraghene12@gmail.com>
2024-03-11 22:57:40 +0100

    [backend] init spring boot app

---
+++ schedulerx_backend/src/main/java/de/schedulerx_backend/SchedulerxBackendApplication.java
@@ -0,0 +1,13 @@
+package de.schedulerx_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerxBackendApplication.class, args);
+    }
+
+}
---
+++ schedulerx_backend/src/test/java/de/schedulerx_backend/SchedulerxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schedulerx_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
